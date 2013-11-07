d7aa0c4e53361797cfef145a57f31d58abcf63e3
dgerbs <dojoh@outlook.com>
2013-11-07 08:28:14 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlashcardsOnRails::Application.config.secret_key_base = '0d1e4ec6f2c638215e33986054ed019f70402e34cd196a43de92a534e6b90be074738e38c1555a051fab03ecb7d4a67f9e34ecb016fe33770f8ed1d34cce2340'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlashcardsOnRails::Application.config.session_store :cookie_store, key: '_flashcards_on_rails_session'
