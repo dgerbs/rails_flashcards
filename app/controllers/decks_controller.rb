1b6c8947cc80496d0fa53aa6814b10f390708114
dgerbs <dojoh@outlook.com>
2013-11-07 11:15:36 -0500

    New form

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -6,4 +6,8 @@ class DecksController < ApplicationController
   def show
     @deck = Deck.find(params[:id])
   end
+
+  def new
+    @deck = Deck.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 FlashcardsOnRails::Application.routes.draw do
+
   root "decks#index"
 
+  get "/decks/new" => "decks#new"
   get "/decks/:id" => "decks#show"
-  # get "/decks" => "decks#index"
+  
 end
