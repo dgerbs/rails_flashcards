23f36919f02fcb6c23767efd157b99b84b15b5be
dgerbs <dojoh@outlook.com>
2013-11-08 14:20:35 -0500

    Complete Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ FlashcardsOnRails::Application.routes.draw do
   resources :decks
   
   root "decks#index"
-
-  get "/decks/new" => "decks#new"
-  get "/decks/:id" => "decks#show"
-  post "/decks" => "decks#create"
 end
